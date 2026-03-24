b90339a6bb5462bcb3838ccc6d19c3220a036768
thjckpka1 <su.vv.amc@gmail.com>
2026-03-24 09:40:18 +0700

    tach css

--- myproject/settings.py
+++ myproject/settings.py
@@ -116,5 +116,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
+STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_URL = 'static/'
 AUTH_USER_MODEL = 'accounts.CustomUser'
